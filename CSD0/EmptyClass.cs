01d77f5345dde1180446661761e7d37a01cb1d96
文彥 <xx48833@gmail.com>
2023-03-06 14:18:22 +0800

    03/06

---
+++ CSD0/EmptyClass.cs
@@ -0,0 +1,39 @@
+﻿public class toSort
+{
+    public IEnumerable<int> ToSort(IEnumerable<int> nu,int begin,int end)
+    {
+        int[]num = nu.ToArray();
+        QuickSort(num, begin, end);
+        IEnumerable<int> sort_Array = num;
+        return sort_Array;
+    }
+    
+    private static void QuickSort(int[] num, int begin, int end)
+    {
+        if (begin >= end) return;
+        int pivotIndex = QuickSort_Once(num, begin, end);
+
+        QuickSort(num, begin, pivotIndex - 1); //往左排列
+        QuickSort(num, pivotIndex + 1, end);    //往右排列
+    }
+    private static int QuickSort_Once(int[] num, int begin, int end)
+    {
+        int pivot = num[begin];
+        int i = begin, j = end;
+        while (i < j)
+        {
+            while (num[j] >= pivot && i < j)
+            {
+                j--;
+            }
+            num[i] = num[j];
+            while (num[i] <= pivot && i < j)
+            {
+                i++;
+            }
+            num[j] = num[i];
+        }
+        num[i] = pivot;
+        return i;
+    }
+}
---
+++ CSD0/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+IEnumerable<int> ArrAy = new[]{ 652, 726, 563, 456, 132, 94, 531, 391, 96, 74, 84, 54, 55, 32, 43 };
+
+int[] array =ArrAy.ToArray();   //轉成一般陣列取長度
+int Len = (array.Length)-1;     
+
+toSort n = new toSort();
+IEnumerable<int>sort_Array = n.ToSort(ArrAy, 0, Len);//丟進去快排回傳一個排完的IEnumerable
+int j = 1;
+foreach(int i in sort_Array)
+{
+    if (i < 100) 
+    {
+        Console.Write($"0{i}");
+    }
+    else
+    {
+        Console.Write($"{i}");
+    }
+    if (j % 2 == 1)
+    {
+        Console.Write("\t");
+    }
+    else
+    {
+        Console.Write("\n");
+    }
+    j++;
+}
+
+
